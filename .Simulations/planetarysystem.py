4fe1e38c89186c334b9397ff5af54891e4db3260
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-05-02 21:10:49 -0300

    update

--- .Simulations/planetarysystem.py
+++ .Simulations/planetarysystem.py
@@ -20,7 +20,7 @@ class Planet:
     AU = 149.6e6 * 1000
     G = 6.67428e-11
     SCALE = 250 / AU
-    TIMESTEP = 3600 * 24  # 1 day
+    TIMESTEP = 3600 * 24
 
     def __init__(self, x, y, radius, color, mass):
         self.x = x
